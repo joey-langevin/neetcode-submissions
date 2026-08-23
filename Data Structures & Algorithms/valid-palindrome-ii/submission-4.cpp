ebeaa8036359dd59a514ee5f5c356f4fe4ced54b
joey-langevin <123837540+joey-langevin@users.noreply.github.com>
2026-08-22 20:11:44 -0500

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool isPal(string& s, int left, int right) {
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+
+
+    bool validPalindrome(string s) {
+        
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+
+            if (s[left] != s[right]) {
+                return isPal(s, left + 1, right) || 
+                        isPal(s, left, right - 1);
+            }
+
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
+
+
