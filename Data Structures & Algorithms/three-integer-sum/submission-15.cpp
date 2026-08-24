d3110610bced296c521fa691033d66a3d6e50d4c
joey-langevin <123837540+joey-langevin@users.noreply.github.com>
2026-08-24 18:34:04 -0500

    Add: three-integer-sum - submission-15

---
+++ Data Structures & Algorithms/three-integer-sum/submission-15.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+
+        for (int i = 0; i < nums.size(); ++i) {
+
+            if (i >0 && nums[i] == nums[i -1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+                if (total == 0) {
+                    output.push_back({nums[left], nums[right], nums[i]});
+                    ++left;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        ++left;
+                    }
+                }
+                else if (total > 0) {
+                    --right;
+                }
+                else {
+                    ++left;
+                }
+            }
+        }
+        return output;
+    }
+};
+
+
+// -1, 0, 0, 1       
+
+// i
+//     l
+//           r
+
+// output : {-1, 0, 1}
+
+// 
+
+
+
